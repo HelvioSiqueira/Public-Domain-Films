58f21af333291a8d459aad3baac2f6ea2d023123
Helvio Siqueira <helviosiqueira@outlook.com>
2024-05-17 17:10:39 -0300

    feat: add shimmer effect

--- app/src/main/java/com/example/publicdomainfilms/ui/presentation/filmList/ItemFilm.kt
+++ app/src/main/java/com/example/publicdomainfilms/ui/presentation/filmList/ItemFilm.kt
@@ -3,13 +3,18 @@ package com.example.publicdomainfilms.ui.presentation.filmList
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
@@ -23,12 +28,22 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -62,6 +77,19 @@ fun SharedTransitionScope.ItemFilm(
     } else if (creatorAny?.javaClass == String::class.java) {
         creator = creatorAny.toString().split(",").first()
     }
+
+//    Card(
+//        modifier = Modifier
+//            .wrapContentSize()
+//            .padding(8.dp)
+//            .defaultMinSize(minHeight = 300.dp),
+//        onClick = { /*TODO*/ }) {
+//        Column(modifier = Modifier
+//            .fillMaxSize()
+//            .shimmerEffect()) {
+//
+//        }
+//    }
 
     Card(
         modifier = modifier
@@ -214,5 +242,35 @@ fun SharedTransitionScope.ItemFilm(
                 }
             }
         }
+    }
+}
+
+fun Modifier.shimmerEffect(): Modifier = composed {
+    var size by remember { mutableStateOf(IntSize.Zero) }
+
+    val transition = rememberInfiniteTransition()
+
+    val startOffsetX by transition.animateFloat(
+        initialValue = -2 * size.width.toFloat(),
+        targetValue = 2 * size.width.toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000
+            )
+        ), label = ""
+    )
+
+    background(
+        brush = Brush.linearGradient(
+            colors = listOf(
+                Color(0xFF999595),
+                MaterialTheme.colorScheme.background,
+                Color(0xFF999595)
+            ),
+            start = Offset(startOffsetX, 0f),
+            end = Offset(startOffsetX + size.width.toFloat(), size.height.toFloat())
+        )
+    ).onGloballyPositioned {
+        size = it.size
     }
 }
