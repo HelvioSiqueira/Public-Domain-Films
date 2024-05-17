454f1ab97b6111e144609b3988ee4d529f745efa
Helvio Siqueira <helviosiqueira@outlook.com>
2024-05-17 16:42:21 -0300

    feat: add circular progress when is loading

--- app/src/main/java/com/example/publicdomainfilms/ui/presentation/filmList/FilmList.kt
+++ app/src/main/java/com/example/publicdomainfilms/ui/presentation/filmList/FilmList.kt
@@ -4,15 +4,20 @@ import android.annotation.SuppressLint
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -24,18 +29,24 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalSharedTransitionApi::class, ExperimentalMaterial3Api::class)
@@ -48,6 +59,8 @@ fun SharedTransitionScope.FilmList(
 
     val listOfFilms by remember { viewModel.listOfFilms }
     var titleAppBar by rememberSaveable { mutableStateOf("COMEDY") }
+
+    val loading by viewModel.isLoading.collectAsState()
 
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
@@ -98,16 +111,28 @@ fun SharedTransitionScope.FilmList(
                         },
                         colors = appBarColors
                     )
-                    LazyVerticalGrid(columns = GridCells.Fixed(2)) {
-                        items(listOfFilms.size) { index ->
-                            val itemFilm = listOfFilms[index]
-                            ItemFilm(
-                                film = itemFilm,
-                                navController = navController,
-                                animatedVisibilityScope = animatedVisibilityScope
-                            )
+
+                    when (loading) {
+                        true -> Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            CircularProgressIndicator()
+                        }
+
+                        false -> LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+                            items(listOfFilms.size) { index ->
+                                val itemFilm = listOfFilms[index]
+                                ItemFilm(
+                                    film = itemFilm,
+                                    navController = navController,
+                                    animatedVisibilityScope = animatedVisibilityScope
+                                )
+                            }
                         }
                     }
+
                 }
             }
         }
